50220c2af5203a0c894707d848f532fc21e4cef0
g00gol <jackey2870@gmail.com>
2023-10-24 13:00:15 -0400

    init

---
+++ client/README.md
@@ -0,0 +1,8 @@
+# React + Vite
+
+This template provides a SUPER minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+```
+npm i -g degit
+npx degit g00gol/vite-super-minimal-react FOLDER_NAME
+```
---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  return (
+    <div>
+      Hello world, this is ultra minimal Vite React!
+      <a href="https://vitejs.dev" target="_blank">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+    </div>
+  );
+}
+
+export default App;
