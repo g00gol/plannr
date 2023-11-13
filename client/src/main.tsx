51de22f5082996f866deb93c8d431c455977e872
Issac <issac.zheng9869@gmail.com>
2023-11-13 12:52:05 -0500

    api key init

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App'
+import App from './App.tsx'
 import './index.css'
 
+import { initializeApp } from "firebase/app";
+
+const config = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
+const app = initializeApp(config);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 )
