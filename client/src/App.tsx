8c80ee6981a7fd92e34e01fbcde1d94d366d1d20
liamsusername <lkpowp@gmail.com>
2023-11-26 18:38:26 -0500

    Added search bar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect}from "react";
 import { PlanMap } from "./components/PlanMap";
 import { PlanMarker } from "./components/PlanMarker";
-import { useJsApiLoader } from '@react-google-maps/api'
+import { Marker, useJsApiLoader } from '@react-google-maps/api'
 
 const elemId = "TEST_MAP";
 
@@ -15,10 +15,23 @@ function App(): React.ReactElement {
   })
 
   return isLoaded ?
-  <PlanMap id={elemId} latitude={40.74691667} longitude={-74.02580556} zoom={15}>
-    <PlanMarker title={"TEST_POINT"} latitude={40.74691667} longitude={-74.02580556}></PlanMarker>
-    <PlanMarker title={"TEST_POINT_2"} latitude={40.74272} longitude={-74.02710}></PlanMarker>
-  </PlanMap>
+  <div>
+    <PlanMap id={elemId} latitude={40.74691667} longitude={-74.02580556} zoom={15}>
+    <form>
+      <div className="flex justify-center ">
+          <input 
+              type="search" 
+              id="searchBar" 
+              placeholder="Search Plannr"
+              className="z-10 opacity-90 block m-3 w-4/6 p-4 ps-10 text-lg border border-gray-600 rounded-xl bg-gray-40 p-50"
+              >
+          </input>
+        </div>
+    </form>
+      <PlanMarker title={"TEST_POINT"} latitude={40.74691667} longitude={-74.02580556}></PlanMarker>
+      <PlanMarker title={"TEST_POINT_2"} latitude={40.74272} longitude={-74.02710}></PlanMarker>
+    </PlanMap>
+  </div>
   : <></>;
 }
 
