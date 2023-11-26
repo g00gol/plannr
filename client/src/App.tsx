0d62f7e97e1c87d1c0f5c829e3cb08302cf8c66f
liamsusername <lkpowp@gmail.com>
2023-11-26 14:28:48 -0500

    WIP Code for directions polyline plotting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,8 +7,10 @@ const elemId = "TEST_MAP";
 
 function App(): React.ReactElement {
 
-  const map = new PlanMap(elemId, 40.71314239501953, -74.00843048095703);
-  const marker = new PlanMarker("TEST_POINT", 40.71314239501953, -74.00843048095703);
+  const map = new PlanMap(elemId, 40.74691667, -74.02580556);
+  const marker = new PlanMarker("TEST_POINT", 40.74691667, -74.02580556);
+  const marker2 = new PlanMarker("TEST_POINT2", 40.74272, -74.02710);
+  map.addLineFromPoints(marker, marker2)
 
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
--- client/src/types/PlanMarker.tsx
+++ client/src/types/PlanMarker.tsx
@@ -1,3 +1,6 @@
+import { Marker } from "@react-google-maps/api"
+import { ReactElement } from "react";
+
 export class PlanMarker {
     private location : [number, number];
 
@@ -10,12 +13,19 @@ export class PlanMarker {
         this.location = [latitude, longitude];
     }
 
-    /*async placeMarker(map : google.maps.Map) : Promise<google.maps.Marker> {
-        const { Marker } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
-        return new Marker ({
-            title: this.title,
-            position: {lat: this.location[0], lng: this.location[1]},
-            map: map
-        });
-    }*/
+    getLatLng() : google.maps.LatLngLiteral {
+        return {
+            lat: this.location[0],
+            lng: this.location[1]
+        }
+    }
+
+    getMarker() : ReactElement<any, any> {
+        return <Marker
+                title={this.title}
+                position= {
+                        this.getLatLng()
+                }
+                ></Marker>
+    }
 }
