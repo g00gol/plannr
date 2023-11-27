dff762ff8c94c8e757d3a38a786977c2c4b3af1e
liamsusername <lkpowp@gmail.com>
2023-11-27 18:06:45 -0500

    Separated types defs file into different type defs

---
+++ client/src/types/PlanMarkerTypes.tsx
@@ -0,0 +1,12 @@
+interface PlanMarkerProps {
+    title?: string
+    latitude?: number
+    longitude?: number
+}
+
+interface PlanMarkerState {
+    title: string,
+    location : google.maps.LatLng
+}
+
+export type { PlanMarkerProps, PlanMarkerState }
---
+++ client/src/types/SearchResTypes.tsx
@@ -0,0 +1,13 @@
+interface SearchResProps {
+    title: string
+    desc: string
+    isOpen: boolean
+    addr: string
+    img?: string
+}
+
+interface SearchResState {
+
+}
+
+export type { SearchResProps, SearchResState }
