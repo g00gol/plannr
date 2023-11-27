728c77f61cdcefd19bd07cd42382a5f718ccbabd
liamsusername <lkpowp@gmail.com>
2023-11-26 19:51:24 -0500

    First attempt at sidebar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect}from "react";
 import { PlanMap } from "./components/PlanMap";
 import { PlanMarker } from "./components/PlanMarker";
 import { Marker, useJsApiLoader } from '@react-google-maps/api'
+import testImage from './assets/testImage.jpeg'
 
 const elemId = "TEST_MAP";
 
@@ -28,6 +29,48 @@ function App(): React.ReactElement {
           </input>
         </div>
     </form>
+    <aside 
+        id="searchResults"
+        className="fixed opacity-80 top-0 left-0 z-20 w-1/6 h-screen transition-transform -translate-x-full sm:translate-x-0"
+        >
+        <div className="z-20 h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-150">
+          <ul className="space-y-[10%]">
+            <li>
+              <div className="grid grid-flow-col grid-rows-2 gap-4">
+                <div className="row-span-2">
+                  <p className="text-lg font-bold">Title</p>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+                <div className="row-span-2 col-span-1">
+                  <img className="object-scale-down rounded-lg" src={testImage}/>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="grid grid-flow-col grid-rows-2 gap-4">
+                <div className="row-span-2">
+                  <p className="text-lg font-bold">Title 2</p>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+                <div className="row-span-2 col-span-1">
+                  <img className="object-scale-down rounded-lg" src={testImage}/>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="grid grid-flow-col grid-rows-2 gap-4">
+                <div className="row-span-2">
+                  <p className="text-lg font-bold">Title 3</p>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+                <div className="row-span-2 col-span-1">
+                  <img className="object-scale-down rounded-lg" src={testImage}/>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+    </aside>
       <PlanMarker title={"TEST_POINT"} latitude={40.74691667} longitude={-74.02580556}></PlanMarker>
       <PlanMarker title={"TEST_POINT_2"} latitude={40.74272} longitude={-74.02710}></PlanMarker>
     </PlanMap>
