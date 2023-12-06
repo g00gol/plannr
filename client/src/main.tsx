ff8c51be326de93f2f466043ce672e6790c68e62
liamsusername <lkpowp@gmail.com>
2023-12-05 20:07:06 -0500

    Fixing branch part 3

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.tsx';
-import './index.css';
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.tsx'
+import './index.css'
 
 //import { initializeApp } from "firebase/app";
 import React from 'react';
