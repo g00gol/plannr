b875c4354404cdd711ad779d8184dd3a8034e644
g00gol <jackeyyang.work@gmail.com>
2023-12-16 18:10:58 -0500

    added typing for TripContext values

--- client/src/contexts/TripContext.tsx
+++ client/src/contexts/TripContext.tsx
@@ -1,19 +1,42 @@
-import React, { useState, useContext, useEffect } from 'react';
-import { TripData } from '../dataObjects/TripData';
-import { AuthContext } from './AuthContext';
+import React, { useState, useContext, useEffect } from "react";
+import { TripData } from "../dataObjects/TripData";
+import { AuthContext } from "./AuthContext";
 
-export const TripContext = React.createContext<TripData>(new TripData("My Trip", []));
+interface TripContextType {
+  currentTrip: TripData;
+  addPlace: (placeId: string) => void;
+  removePlace: (placeId: string) => void;
+}
+
+export const TripContext = React.createContext<TripContextType>(null!);
 
 // create functions for mutating tripcontext here; will use replace instead of mutation for statefulness
 export const TripProvider = ({ children }: React.PropsWithChildren<{}>) => {
-    const [currentTrip, setCurrentTrip] = useState<TripData>(new TripData("My Trip", []));
-    const [loadingTrip, setLoadingTrip] = useState<boolean>(true);
-    const currentUser = useContext(AuthContext);
-    useEffect(() => {
-        // load trip if user has trip in session
-    }, []);
-    
-    return (
-        <TripContext.Provider value={currentTrip}>{children}</TripContext.Provider>
-    )
-}
+  const [currentTrip, setCurrentTrip] = useState<TripData>(
+    new TripData("My Trip", [])
+  );
+  const [loadingTrip, setLoadingTrip] = useState<boolean>(true);
+  const currentUser = useContext(AuthContext);
+
+  useEffect(() => {
+    // load trip if user has trip in session
+  }, []);
+
+  const addPlace = (placeId: string) => {
+    currentTrip.place_ids.push(placeId);
+    setCurrentTrip(currentTrip);
+  };
+
+  const removePlace = (placeId: string) => {
+    currentTrip.place_ids = currentTrip.place_ids.filter(
+      (id) => id !== placeId
+    );
+    setCurrentTrip(currentTrip);
+  };
+
+  return (
+    <TripContext.Provider value={{ currentTrip, addPlace, removePlace }}>
+      {children}
+    </TripContext.Provider>
+  );
+};
