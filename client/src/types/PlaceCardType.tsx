04b8ab1b876336002a213f89956483c08a932707
g00gol <jackeyyang.work@gmail.com>
2023-12-17 12:42:22 -0500

    run prettier . --write on all files

--- client/src/types/PlaceCardType.tsx
+++ client/src/types/PlaceCardType.tsx
@@ -1,10 +1,10 @@
 import { PlaceData } from "../dataObjects/PlaceData";
 
 interface PlaceCardProps {
-    mapRef: React.MutableRefObject<google.maps.Map | undefined>
-    place: PlaceData
-    isResult: boolean
-    children?: React.ReactNode
+  mapRef: React.MutableRefObject<google.maps.Map | undefined>;
+  place: PlaceData;
+  isResult: boolean;
+  children?: React.ReactNode
 }
 
 interface PlaceCardState {
