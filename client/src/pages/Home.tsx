c6daf60678178021bf03c5b54767f5b92aa1c806
liamsusername <lkpowp@gmail.com>
2023-12-18 16:25:41 -0500

    Added mutually exclusivity between marker types

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -51,8 +51,8 @@ export default function Home(props: HomeProps): React.ReactElement {
   const [travelMode, setTravelMode] = useState<google.maps.TravelMode>();
   const [keyWordData, setKeyWordData] = useState<string>("");
   const [searchText, setSearchText] = useState<string>("");
-  const { currentInfoWindow, setInfoWindow} = useContext(SearchResultContext);
-  const { currentTrip } = useContext(TripContext);
+  const { currentInfoWindow, setInfoWindow : setSearchWindow } = useContext(SearchResultContext);
+  const { currentTrip, setInfoWindow : setTripWindow } = useContext(TripContext);
 
   const { currentTrip } = React.useContext(TripContext);
 
@@ -67,6 +67,11 @@ export default function Home(props: HomeProps): React.ReactElement {
     setPlaceData([]);
     setMarkerData([]);
     mapRef.current = map;
+  }, []);
+
+  const updateWindows = useCallback((index: number) => {
+    setSearchWindow(index);
+    setTripWindow(-1);
   }, []);
 
   const search = (event: FormEvent<HTMLFormElement>) => {
@@ -203,7 +208,7 @@ export default function Home(props: HomeProps): React.ReactElement {
 
             {currentInfoWindow != -1 ?
               <InfoWindow
-                onCloseClick={() => setInfoWindow(-1)}
+                onCloseClick={() => setSearchWindow(-1)}
                 options={{
                   ariaLabel: placeData[currentInfoWindow].title,
                   position: placeData[currentInfoWindow].marker?.location,
@@ -250,7 +255,7 @@ export default function Home(props: HomeProps): React.ReactElement {
                       color: "black",
                       text: String(ind),
                     }}
-                    onClick={() => setInfoWindow(ind)}
+                    onClick={() => updateWindows(ind)}
                     icon={{
                       path: pinSVGFilled,
                       anchor: new google.maps.Point(12, 17),
