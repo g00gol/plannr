9dcc99fd51a6437cde6a6fd18e1f43ab50aea6b6
iscii <issac.zheng9869@gmail.com>
2024-01-01 18:40:13 -0500

    Moved uvicorn init back to main.py, trying above startup. Missing variable `handler` or `app` in file "main.py"

--- server/main.py
+++ server/main.py
@@ -1,4 +1,36 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from routers import users_router
+from db import startup, shutdown
+from typing import cast
 import uvicorn
+import os
 
 if __name__ == "__main__":
-    uvicorn.run("api:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    
+app = FastAPI()
+CORS_URL = cast(str, os.getenv("MONGO_URI"))
+origins = [
+    CORS_URL
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(users_router)
+
+@app.on_event("startup")
+async def on_startup():
+    await startup()
+
+
+@app.on_event("shutdown")
+async def on_shutdown():
+    await shutdown()
