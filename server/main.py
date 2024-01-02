43adafa04ef842812de657d0bd0e56479ed507c5
iscii <issac.zheng9869@gmail.com>
2024-01-01 20:43:59 -0500

    fix a typo for CORS

--- server/main.py
+++ server/main.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
     
 app = FastAPI()
-CORS_URL = cast(str, os.getenv("MONGO_URI"))
+CORS_URL = cast(str, os.getenv("CORS_URL"))
 origins = [
     CORS_URL
 ]
